dc987a602ee8cbfc7d0b302e496722c7c93c6650
Christpher Roand <mumadarif99@gmail.com>
2024-02-08 19:10:31 +0800

    modified:   node_modules/.package-lock.json 	new file:   node_modules/express-session/HISTORY.md 	new file:   node_modules/express-session/LICENSE 	new file:   node_modules/express-session/README.md 	new file:   node_modules/express-session/index.js 	new file:   node_modules/express-session/node_modules/cookie-signature/History.md 	new file:   node_modules/express-session/node_modules/cookie-signature/Readme.md 	new file:   node_modules/express-session/node_modules/cookie-signature/index.js 	new file:   node_modules/express-session/node_modules/cookie-signature/package.json 	new file:   node_modules/express-session/node_modules/cookie/HISTORY.md 	new file:   node_modules/express-session/node_modules/cookie/LICENSE 	new file:   node_modules/express-session/node_modules/cookie/README.md 	new file:   node_modules/express-session/node_modules/cookie/SECURITY.md 	new file:   node_modules/express-session/node_modules/cookie/index.js 	new file:   node_modules/express-session/node_modules/cookie/package.json 	new file:   node_modules/express-session/node_modules/depd/History.md 	new file:   node_modules/express-session/node_modules/depd/LICENSE 	new file:   node_modules/express-session/node_modules/depd/Readme.md 	new file:   node_modules/express-session/node_modules/depd/index.js 	new file:   node_modules/express-session/node_modules/depd/lib/browser/index.js 	new file:   node_modules/express-session/node_modules/depd/package.json 	new file:   node_modules/express-session/node_modules/safe-buffer/LICENSE 	new file:   node_modules/express-session/node_modules/safe-buffer/README.md 	new file:   node_modules/express-session/node_modules/safe-buffer/index.d.ts 	new file:   node_modules/express-session/node_modules/safe-buffer/index.js 	new file:   node_modules/express-session/node_modules/safe-buffer/package.json 	new file:   node_modules/express-session/package.json 	new file:   node_modules/express-session/session/cookie.js 	new file:   node_modules/express-session/session/memory.js 	new file:   node_modules/express-session/session/session.js 	new file:   node_modules/express-session/session/store.js 	new file:   node_modules/random-bytes/HISTORY.md 	new file:   node_modules/random-bytes/LICENSE 	new file:   node_modules/random-bytes/README.md 	new file:   node_modules/random-bytes/index.js 	new file:   node_modules/random-bytes/package.json 	new file:   node_modules/uid-safe/HISTORY.md 	new file:   node_modules/uid-safe/LICENSE 	new file:   node_modules/uid-safe/README.md 	new file:   node_modules/uid-safe/index.js 	new file:   node_modules/uid-safe/package.json 	modified:   package-lock.json 	modified:   package.json 	modified:   public/login.html 	modified:   routes/api.js 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,43 @@
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
+var uudi = require('uuid');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+// Setup express-session middleware
+router.use(session({
+  secret: uudi.v4,
+  resave: false,
+  saveUninitialized: true
+}));
+
+router.get('/login', (req, res) => {
+  res.sendFile('login.html', ({
+    root: 'public'
+  }));
 });
 
+// Define checkAuthentication middleware
+function checkAuthentication(req, res, next) {
+  if (req.session && req.session.user) {
+    // Jika pengguna sudah login, lanjutkan ke rute berikutnya
+    return next();
+  } else {
+    // Jika pengguna belum login, redirect ke halaman login
+    return res.redirect('/login');
+  }
+}
+
+// Define renderHtml function
 function renderHtml(url, file) {
-  router.get(url, function (req, res) {
-    res.sendFile(file, ({
+  router.get(url, checkAuthentication, function (req, res) {
+    res.sendFile(file, {
       root: 'public'
-    }));
+    });
   });
 };
 
+// Setup routes
+renderHtml('/', 'index.html');
 renderHtml('/kodeAkun', 'code_account.html');
 renderHtml('/labaRugi', 'jurnal_labaRugi.html');
 renderHtml('/dataPinjaman', 'data_pinjaman.html');

387ab998adecce2469c91b2e4b715a024c0fbcc7
ChrishtoherRolad132 <mumadarif99@gmail.com>
2024-02-17 16:18:36 +0800

    modified:   public/code_account.html 	new file:   public/dashboard.html 	modified:   public/data/data_pinjaman.json 	modified:   public/data_pinjaman.html 	modified:   public/data_simpanan.html 	modified:   public/index.html 	modified:   public/jurnal_bukuBesar.html 	modified:   public/jurnal_harian.html 	modified:   public/jurnal_labaRugi.html 	modified:   public/jurnal_neracaSaldo.html 	modified:   public/jurnal_umum.html 	deleted:    public/login.html 	modified:   routes/api.js 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,10 @@ var session = require('express-session');
 var router = express.Router();
 var uudi = require('uuid');
 
-// Setup express-session middleware
-router.use(session({
-  secret: uudi.v4,
-  resave: false,
-  saveUninitialized: true
-}));
-
-router.get('/login', (req, res) => {
-  res.sendFile('login.html', ({
-    root: 'public'
-  }));
-});
-
-// Define checkAuthentication middleware
-function checkAuthentication(req, res, next) {
-  if (req.session && req.session.user) {
-    // Jika pengguna sudah login, lanjutkan ke rute berikutnya
-    return next();
-  } else {
-    // Jika pengguna belum login, redirect ke halaman login
-    return res.redirect('/login');
-  }
-}
 
 // Define renderHtml function
 function renderHtml(url, file) {
-  router.get(url, checkAuthentication, function (req, res) {
+  router.get(url, function (req, res) {
     res.sendFile(file, {
       root: 'public'
     });
@@ -38,6 +15,7 @@ function renderHtml(url, file) {
 
 // Setup routes
 renderHtml('/', 'index.html');
+renderHtml('/dashboard', 'dashboard.html');
 renderHtml('/kodeAkun', 'code_account.html');
 renderHtml('/labaRugi', 'jurnal_labaRugi.html');
 renderHtml('/dataPinjaman', 'data_pinjaman.html');
